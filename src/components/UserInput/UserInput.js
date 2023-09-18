5d9a8ed09606f5d146c1679bdcbd41302e3322ac
Hanif Omar <hanif.omarr@gmail.com>
2023-09-18 16:40:41 +0800

    Split into core components

---
+++ src/components/UserInput/UserInput.js
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+
+function UserInput({ submitUserInput }) {
+  return (
+    <div>
+      <form className="form" onSubmit={submitHandler}>
+        <div className="input-group">
+          <p>
+            <label htmlFor="current-savings">Current Savings ($)</label>
+            <input
+              onChange={currentSavingHandler}
+              type="number"
+              id="current-savings"
+            />
+          </p>
+          <p>
+            <label htmlFor="yearly-contribution">Yearly Savings ($)</label>
+            <input
+              onChange={yearlySavingHandler}
+              type="number"
+              id="yearly-contribution"
+            />
+          </p>
+        </div>
+        <div className="input-group">
+          <p>
+            <label htmlFor="expected-return">
+              Expected Interest (%, per year)
+            </label>
+            <input
+              onChange={interestHandler}
+              type="number"
+              id="expected-return"
+            />
+          </p>
+          <p>
+            <label htmlFor="duration">Investment Duration (years)</label>
+            <input onChange={durationHandler} type="number" id="duration" />
+          </p>
+        </div>
+        <p className="actions">
+          <button onClick={resetHandler} type="reset" className="buttonAlt">
+            Reset
+          </button>
+          <button type="submit" className="button">
+            Calculate
+          </button>
+        </p>
+      </form>
+    </div>
+  );
+}
+
+export default UserInput;
